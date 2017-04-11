4d0391b4278a029ae04779033d35ae5a9978a939
494873674@qq.com <zhenhuadeng@zhenhuadeMacbookdeMacBook-Pro.local>
2017-04-11 11:21:11 +0800

    add Array.lastIndexOf

--- polyfill/js/Array.lastIndexOf.js
+++ polyfill/js/Array.lastIndexOf.js
@@ -1,10 +1,10 @@
-Array.prototype.indexOf = Array.indexOf || function(searchElement, fromIndex) {
+Array.prototype.lastIndexOf = Array.lastIndexOf || function(searchElement, fromIndex) {
     var o = Object(this),
         len = o.length >> 0,
         formatIndex = fromIndex >> 0,
         index = formatIndex >= 0 ? Math.min(formatIndex, len) : Math.max(formatIndex+len, 0);
 
-    for (var i=index;i<len;i++) {
+    for (var i=len;i>=index;i--) {
         if (o[i] === searchElement || (isNaN(searchElement) && isNaN(o[i]))) {
             return i;
         }
